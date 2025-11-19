77a8cd1d49e6add3a6d80a231075a1a14855ba34
andrii-oss <andrii@velvion.com.br>
2025-11-19 18:40:04 +0200

    fix tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,10 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+    <div className="min-w-full h-full flex flex-col items-center bg-gray-600">
+      <main className="flex flex-col items-center flex-1 w-full h-full my-14 bg-gray-400">
+        <div className="flex w-full flex-col justify-between gap-10 container mx-auto p-14">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
@@ -34,31 +26,31 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <div className={styles.ctas}>
+            <a
+              className={styles.primary}
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                className={styles.logo}
+                src="/vercel.svg"
+                alt="Vercel logomark"
+                width={16}
+                height={16}
+              />
+              Deploy Now
+            </a>
+            <a
+              className={styles.secondary}
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Documentation
+            </a>
+          </div>
         </div>
       </main>
     </div>
---
+++ tailwind.config.ts
@@ -0,0 +1,47 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./modules/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // default breakpoints but with 40px removed
+      screens: {
+        "2xl": "1440px",
+      },
+    },
+    // custom begins here
+    extend: {
+      screens: {
+        // custom screens for most popular usages
+        smobile: "375px",
+        mobile: "428px",
+        lmobile: "600px",
+        tablet: "768px",
+        ltablet: "920px",
+        laptop: "1280px",
+        desktop: "1440px",
+        bdesktop: "1728px",
+      },
+      fontFamily: {
+        // geistSans: ["var(--font-geist-sans)"],
+        // geistMono: ["var(--font-geist-mono)"],
+        // firaSans: ["var(--font-fira-sans)"],
+      },
+      transform: {
+        "translate-center": "translate(-50%, -50%)",
+      },
+      colors: {},
+      borderRadius: {},
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  // plugins: [require("tailwindcss-animate")],
+} satisfies Config;
